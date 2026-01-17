df51aed3c3deb76c36659dee21b5b7913f9f82f7
heybloxer <ebcbdubshab@gmail.com>
2026-01-18 00:57:33 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,14 @@
-print("hello world dont use this script. this script on work. ITS A TEST!")
+--[[
+__________                      .__.__  .__  .__                __________.__                             
+\______   \____________  _______|__|  | |  | |__|____    ____   \______   \  |   _______  ___ ___________ 
+ |    |  _/\_  __ \__  \ \___   /  |  | |  | |  \__  \  /    \   |    |  _/  |  /  _ \  \/  // __ \_  __ \
+ |    |   \ |  | \// __ \_/    /|  |  |_|  |_|  |/ __ \|   |  \  |    |   \  |_(  <_> >    <\  ___/|  | \/
+ |______  / |__|  (____  /_____ \__|____/____/__(____  /___|  /  |______  /____/\____/__/\_ \\___  >__|   
+        \/             \/      \/                    \/     \/          \/                 \/    \/        
+                         		   Welcome to Brazillian Bloxer Scripts!
+						             			Github: github.com/brazillianbloxer
+									                    Universal Script
+                                    USE AT YOUR OWN RISK
+]]--
+loadstring(game:HttpGet("https://raw.githubusercontent.com/brazillianbloxer/UniversalScript/refs/heads/main/loader.lua"))()
+
